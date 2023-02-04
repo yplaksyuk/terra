13921e51fa32b8887a7e185d7a5ffcbe86404d42
Yuri Plaksyuk <yuri.plaksyuk@chestnutcorp.com>
2023-02-04 12:42:52 +0200

    Relocate to docs/

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const PORT = 3000;
 const express = require('express');
 const app = express();
 
-app.get('/*', express.static(`./site`));
+app.get('/*', express.static(`./docs`));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
