f866b2fd519813d6223ae4c4f8c9a0750e0686c9
Yuri Plaksyuk <yuri.plaksyuk@chestnutcorp.com>
2023-02-08 19:43:48 +0200

    Use HTTPS for local server - required by some advanced Web API.

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-const PORT = 3000;
+const PORT = 8443;
 
 const express = require('express');
-const app = express();
+const https = require('https');
+const fs = require('fs');
 
+const creds = {
+	key: fs.readFileSync('selfsigned.key', 'utf8'),
+	cert: fs.readFileSync('selfsigned.crt', 'utf8')
+};
+
+const app = express();
 app.get('/*', express.static(`./docs`));
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+const server = https.createServer(creds, app);
+server.listen(PORT);
+
+console.log(`Server started on port ${PORT}`);
